2f304bece87b884434728c6b6f36a420ffa27c25
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-25 10:52:54 -0300

    chore: add tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1 className="items-center justify-normal text-red-500">
+        Hello, World!
+      </h1>
     </div>
   )
 }
