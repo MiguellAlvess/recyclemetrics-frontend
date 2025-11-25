4927eab8b66c4034e594207fbbb6089f163c6378
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-25 11:35:28 -0300

    chore: add react router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes } from 'react-router'
+import { Route } from 'react-router'
+import NotFoundPage from './pages/notfound'
+import SignupPage from './pages/signup'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="sign-up" element={<SignupPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
