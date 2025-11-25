0680c66fc48a5143d047909a34e2f3ba52dc3d98
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-25 20:15:14 -0300

    feat: add validation to signup form

--- src/components/password-input.tsx
+++ src/components/password-input.tsx
@@ -1,30 +1,40 @@
-import { useState } from 'react'
-import { Input } from './ui/input'
-import { Button } from './ui/button'
-import { EyeIcon, EyeOff } from 'lucide-react'
+import { Eye, EyeOff } from 'lucide-react'
+import { forwardRef, useState } from 'react'
 
-interface PasswordInputProps {
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+
+interface PasswordInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder: string
 }
 
-const PasswordInput = ({ placeholder }: PasswordInputProps) => {
-  const [passwordIsVisible, setPasswordIsVisible] = useState(false)
-  return (
-    <div className="relative">
-      <Input
-        type={passwordIsVisible ? 'text' : 'password'}
-        placeholder={placeholder}
-      />
-      <Button
-        type="button"
-        variant="ghost"
-        className="absolute right-2 top-1/2 h-8 w-8 -translate-y-1/2 p-0 text-muted-foreground"
-        onClick={() => setPasswordIsVisible((prevValue) => !prevValue)}
-      >
-        {passwordIsVisible ? <EyeOff size={18} /> : <EyeIcon size={18} />}
-      </Button>
-    </div>
-  )
-}
+const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
+  ({ placeholder, ...props }, ref) => {
+    const [passwordIsVisible, setPasswordIsVisible] = useState(false)
+
+    return (
+      <div className="relative">
+        <Input
+          ref={ref}
+          type={passwordIsVisible ? 'text' : 'password'}
+          placeholder={placeholder}
+          {...props}
+        />
+
+        <Button
+          type="button"
+          variant="ghost"
+          className="absolute right-2 top-1/2 h-8 w-8 -translate-y-1/2 p-0 text-muted-foreground"
+          onClick={() => setPasswordIsVisible((prev) => !prev)}
+        >
+          {passwordIsVisible ? <EyeOff size={18} /> : <Eye size={18} />}
+        </Button>
+      </div>
+    )
+  }
+)
+
+PasswordInput.displayName = 'PasswordInput'
 
 export default PasswordInput
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,3 +1,10 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router'
+import z from 'zod'
+
+import signupImage from '@/assets/images/signup-page-image.svg'
+import PasswordInput from '@/components/password-input'
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -7,40 +14,136 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { Link } from 'react-router'
-import signupImage from '@/assets/images/signup-page-image.svg'
-import PasswordInput from '@/components/password-input'
+
+const signupSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: 'Nome é obrigatório',
+  }),
+  email: z
+    .string()
+    .email({
+      message: 'Email inválido',
+    })
+    .min(1, {
+      message: 'Email é obrigatório',
+    }),
+  password: z.string().trim().min(8, {
+    message: 'Senha deve ter no mínimo 8 caracteres',
+  }),
+  confirmPassword: z.string().trim().min(8, {
+    message: 'Confirmação de senha deve ser igual a senha',
+  }),
+})
 
 const SignupPage = () => {
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  })
+
+  const handleSubmit = (data: z.infer<typeof signupSchema>) => {
+    console.log(data)
+  }
   return (
     <div className="flex min-h-screen w-full">
       <div className="flex w-full items-center justify-center px-4 md:w-1/2">
-        <Card className="w-full max-w-md shadow-lg">
-          <CardHeader className="flex items-center justify-center text-center">
-            <CardTitle>Crie sua conta</CardTitle>
-            <CardDescription>
-              Comece sua jornada rumo a um estilo de vida mais sustentável
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <Input placeholder="Digite seu nome" />
-            <Input placeholder="Digite seu email" />
-            <PasswordInput placeholder="Digite sua senha" />
-            <PasswordInput placeholder="Confirme sua senha" />
-          </CardContent>
-          <CardFooter className="flex flex-col gap-2">
-            <Button type="submit" className="w-full">
-              Criar conta
-            </Button>
-            <div className="flex items-center justify-center text-sm">
-              <p className="text-muted-foreground">Já tem uma conta?</p>
-              <Button variant="link" className="ml-1 p-0 text-sm" asChild>
-                <Link to="/login">Faça login</Link>
-              </Button>
-            </div>
-          </CardFooter>
-        </Card>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleSubmit)}>
+            <Card className="w-full max-w-md shadow-lg">
+              <CardHeader className="flex items-center justify-center text-center">
+                <CardTitle>Crie sua conta</CardTitle>
+                <CardDescription>
+                  Comece sua jornada rumo a um estilo de vida mais sustentável
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Nome</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Digite seu nome" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Digite seu email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Senha</FormLabel>
+                      <FormControl>
+                        <PasswordInput
+                          placeholder="Digite sua senha"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Confirme sua senha</FormLabel>
+                      <FormControl>
+                        <PasswordInput
+                          placeholder="Confirme sua senha"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </CardContent>
+              <CardFooter className="flex flex-col gap-2">
+                <Button type="submit" className="w-full">
+                  Criar conta
+                </Button>
+                <div className="flex items-center justify-center text-sm">
+                  <p className="text-muted-foreground">Já tem uma conta?</p>
+                  <Button variant="link" className="ml-1 p-0 text-sm" asChild>
+                    <Link to="/login">Faça login</Link>
+                  </Button>
+                </div>
+              </CardFooter>
+            </Card>
+          </form>
+        </Form>
       </div>
       <div className="hidden md:block md:w-1/2">
         <img
