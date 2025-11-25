576896a19ffc70b5c5a4944dd4862227317d5374
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-25 10:59:19 -0300

    chore: add shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from './components/ui/button'
+
 const App = () => {
   return (
     <div>
       <h1 className="items-center justify-normal text-red-500">
         Hello, World!
       </h1>
+      <Button>Testes</Button>
     </div>
   )
 }
