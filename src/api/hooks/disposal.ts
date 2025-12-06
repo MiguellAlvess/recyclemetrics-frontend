53fa6fe6c8dfb969df9758ea0b1a88a24db494a6
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-12-06 10:23:03 -0300

    fix: correct typings and return value in disposals query hook

--- src/api/hooks/disposal.ts
+++ src/api/hooks/disposal.ts
@@ -3,6 +3,7 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import type { CreateDisposalSchema } from '@/forms/schemas/disposal'
 
 import { DisposalService } from '../services/disposal/disposal'
+import type { CreateDisposalResponse } from '../services/disposal/types'
 
 export const useCreateDisposal = () => {
   const queryClient = useQueryClient()
@@ -16,7 +17,7 @@ export const useCreateDisposal = () => {
 }
 
 export const useGetDisposals = () => {
-  return useQuery({
+  return useQuery<CreateDisposalResponse[]>({
     queryKey: ['disposals'],
     queryFn: () => DisposalService.getAll(),
   })
