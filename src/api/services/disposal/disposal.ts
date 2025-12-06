14f3f414ce58d583a1c95380a35b73152ba2e8ea
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-12-06 10:13:09 -0300

    feat: add getAll funciton to disposal service

--- src/api/services/disposal/disposal.ts
+++ src/api/services/disposal/disposal.ts
@@ -7,4 +7,8 @@ export const DisposalService = {
     const response = await protectedApi.post('/disposals', input)
     return response.data
   },
+  async getAll(): Promise<CreateDisposalResponse[]> {
+    const response = await protectedApi.get('/disposals')
+    return response.data
+  },
 }
