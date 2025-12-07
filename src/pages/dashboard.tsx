3826ef01032b92f8ff58b27d8214f610d1acd04a
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-12-07 10:29:18 -0300

    feat: add initial dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,34 @@
+import {
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from '@/components/page-container'
+import StatsCards from '@/components/stats-card'
+
 const DashboardPage = () => {
-  return <div>Dashboard Page - Protected Content</div>
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Dashboard</PageTitle>
+          <PageDescription>
+            Métricas das suas compras e descartes dos últimos 30 dias
+          </PageDescription>
+        </PageHeaderContent>
+      </PageHeader>
+      <PageContent>
+        <StatsCards
+          totalDisposals={10}
+          recyclingPercentage={15}
+          totalPurchase={10}
+          mostUsedDestination="Reciclagem"
+        />
+      </PageContent>
+    </PageContainer>
+  )
 }
 
 export default DashboardPage
